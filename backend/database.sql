20bd9e443a79bd124c3a94a21b5e74c7a20df6b4
Ziolson <ziolson92@gmail.com>
2016-11-17 17:08:03 +0100

    Database script v2

--- backend/database.sql
+++ backend/database.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS currency (
-  currency_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   currency_name VARCHAR(3) NOT NULL,
   buy_price DOUBLE,
   sell_price DOUBLE
 );
 
 CREATE TABLE IF NOT EXISTS location (
-  location_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   streed_adress VARCHAR(30) NOT NULL ,
   postal_code VARCHAR(6) NOT NULL ,
   city VARCHAR(30) NOT NULL ,
@@ -14,18 +14,18 @@ CREATE TABLE IF NOT EXISTS location (
 );
 
 CREATE TABLE IF NOT EXISTS account (
-  account_id SERIAL PRIMARY KEY ,
+  id SERIAL PRIMARY KEY ,
   owner_id BIGINT UNSIGNED NOT NULL,
-  account_tumber VARCHAR(26) NOT NULL ,
+  account_tumber VARCHAR(26) UNIQUE NOT NULL ,
   balance DOUBLE DEFAULT 0 NOT NULL ,
-  creation_time TIMESTAMP DEFAULT now() NOT NULL,
-  modification_time TIMESTAMP DEFAULT now() NOT NULL
+  creationtime TIMESTAMP DEFAULT now() NOT NULL,
+  modificationtime TIMESTAMP DEFAULT now() NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS user (
-  user_id SERIAL PRIMARY KEY ,
+  id SERIAL PRIMARY KEY ,
   account_id BIGINT UNSIGNED NOT NULL ,
-  username VARCHAR(30) NOT NULL ,
+  username VARCHAR(30) UNIQUE NOT NULL ,
   password VARCHAR(30) NOT NULL ,
   name VARCHAR(30) NOT NULL ,
   surname VARCHAR(30) NOT NULL ,
@@ -35,11 +35,11 @@ CREATE TABLE IF NOT EXISTS user (
   FOREIGN KEY (location_id) REFERENCES location(location_id)
 );
 
-CREATE TABLE IF NOT EXISTS transaction (
-  transaction_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS transfer (
+  id SERIAL PRIMARY KEY,
   sender_id BIGINT UNSIGNED NOT NULL ,
   receiver_id BIGINT UNSIGNED NOT NULL ,
-  transaction_time TIMESTAMP DEFAULT now() NOT NULL ,
+  transactiontime TIMESTAMP DEFAULT now() NOT NULL ,
   currency_id BIGINT UNSIGNED NOT NULL ,
   amount DOUBLE NOT NULL ,
   description VARCHAR(150) NOT NULL,
