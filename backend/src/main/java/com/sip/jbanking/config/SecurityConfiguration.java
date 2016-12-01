b6b0a19443a2ca712ceb34abf028aa2a2eb657da
notechus <seba0713@gmail.com>
2016-12-01 23:47:57 +0100

    CORS finally working on Stormpath shitty /oauth/token

--- backend/src/main/java/com/sip/jbanking/config/SecurityConfiguration.java
+++ backend/src/main/java/com/sip/jbanking/config/SecurityConfiguration.java
@@ -1,10 +1,17 @@
 package com.sip.jbanking.config;
 
+import com.stormpath.sdk.servlet.filter.StormpathFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpath;
 
@@ -13,14 +20,32 @@ import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpa
  */
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 @EnableTransactionManagement
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.apply(stormpath());
-
         http.authorizeRequests()
-                .antMatchers("/api/v1/account", "api/v1/transfer").fullyAuthenticated();
+                .antMatchers("/api/v1/account", "api/v1/transfer").fullyAuthenticated()
+                .antMatchers("/login", "/me", "/css/**", "/js/**").permitAll();
+
+        http.csrf().disable();
+        http.cors().disable();
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
